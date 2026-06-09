b6a84ea5fa287837ff2026c2a0b3b31175efec33
Göran Olson <goran@famolson.se>
2026-06-09 10:48:33 +0200

    Add project files.

---
+++ dotnet_exercise_csharp_5_garage_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_exercise_csharp_5_garage_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till Garage 1.0");
+        }
+    }
+}
