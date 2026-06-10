f4b4b8be72ec57811e5fc9e148ae5e314ddec46b
Göran Olson <goran@famolson.se>
2026-06-10 13:15:52 +0200

    Created class Garage and added temp tests

---
+++ dotnet_exercise_csharp_5_garage_1/Classes/Garage.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace dotnet_exercise_csharp_5_garage_1.Classes
+{
+    internal class Garage
+    {
+        private Vehicle[] _parking = new Vehicle[10];
+
+        private int _capacity;  // Available parkings
+        private int _count;  // Occupied seats
+
+        public int Capacity => _capacity;
+        public int Count => _count;
+
+        public bool IsFull => _count >= _capacity;
+
+        public Garage(int capacity)
+        {
+            _capacity = capacity;
+            Console.WriteLine($"Garage({_capacity})");
+        }
+
+        public bool ParkCar(Vehicle vehicle)
+        {
+            bool success = false;
+
+            if (!IsFull)
+            {
+                int freePos = GetEmptyParking();
+                if (freePos >= 0)
+                {
+                    Console.WriteLine($"freePos: {freePos}");
+                    _parking[freePos] = vehicle;
+                    _count++;
+                    success = true;
+                }
+                else
+                {
+                    Console.WriteLine("No free space");
+                }
+            }
+
+            return success;
+        }
+
+        public bool UnparkCar(Vehicle vehicle)
+        {
+            bool success = false;
+
+            for (int i = 0; i < _parking.Length; i++)
+            {
+                if (_parking[i] != null)
+                {
+                    // Console.WriteLine($" # {i} - {_parking[i].RegNbr}");
+                    if (_parking[i].RegNbr.ToUpper() == vehicle.RegNbr.ToUpper())
+                    {
+                         Console.WriteLine("Unparking RegNbr.: " + vehicle.RegNbr);
+
+                        // ToDo - null, 
+                        _parking[i] = null;  
+                        _count--;
+                        success = true;
+                    }
+                }
+            }
+
+            return success;
+        }
+        
+
+        public void PrintParkedVehicles()
+        {
+            Console.WriteLine($"{Environment.NewLine}___ Parked vehicles ___ ({Count}/{Capacity})");
+            if (_count > 0)
+            {
+                for (int i = 0; i < _parking.Length; i++)
+                {
+                    if (_parking[i] != null)
+                    {
+                        Console.WriteLine("- " + _parking[i].ToString());
+                    }
+                }
+            }
+            else
+            {
+                //Console.WriteLine($"{Environment.NewLine}Garaget är tomt!");
+                Console.WriteLine($"Garaget är tomt!");
+            }
+            
+            Console.WriteLine();
+        }
+
+        private int GetEmptyParking()
+        {
+            int n = -1;
+
+            // int numIndex = Array.IndexOf(numbers, numToRemove);
+            for (int i = 0; i < _parking.Length; i++)
+            {
+                if (_parking[i] is null)
+                {
+                    n = i;
+                    break;
+                }
+            }
+
+
+            return n;
+        }
+    }
+}
--- dotnet_exercise_csharp_5_garage_1/Program.cs
+++ dotnet_exercise_csharp_5_garage_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using dotnet_exercise_csharp_5_garage_1.Classes;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
 
 namespace dotnet_exercise_csharp_5_garage_1
@@ -12,29 +13,120 @@ namespace dotnet_exercise_csharp_5_garage_1
             Console.WriteLine($"========================={Environment.NewLine}");
             // Get garage size
 
+            //Vehicle[] garage2 = new Vehicle[12];
 
             // SeedData
             // Test-data
             Airplane airplane = new Airplane("SN 1234", "silver", 16, 50, 4);           
             Boat boat = new Boat("Matilda", "Vit", 0, 65.5, 2);
             Bus bus = new Bus("EEE001", "Vit", 6, 9.8, 40);
-            
             Car car = new ("ABC123", "Röd", 4, "Gasoline", 2100);
-
+            Car car2 = new("ZZZ123", "Blå", 4, "Gasoline", 2100);
             Motorcycle motorcycle = new("MNO987", "Svart", 2, 2, 1300.5);
 
+            //garage2.Append(airplane);  // 
 
             Console.WriteLine();
             Console.WriteLine("> " + airplane.ToString());
             Console.WriteLine("> " + bus.ToString());
             Console.WriteLine("> " + boat.ToString());
             Console.WriteLine("> " + car.ToString());
+            Console.WriteLine("> " + car2.ToString());
             Console.WriteLine("> " + motorcycle.ToString());
 
             // Program menu
 
+            Garage garage = new(2);
+            Console.WriteLine($"Garage size: " + garage.Capacity);
+            Console.WriteLine($"Garage count: " + garage.Count);
+            Console.WriteLine($"Garage IsFull: " + garage.IsFull);
+            Console.WriteLine();
+
+            if (!garage.ParkCar(car))
+                Console.WriteLine("Failed parked car");
+            Console.WriteLine($"Garage count: " + garage.Count);
+            Console.WriteLine();
+
+            if (!garage.ParkCar(car2))
+                Console.WriteLine("Failed parked car2");
+            Console.WriteLine($"Garage count: " + garage.Count);
+            Console.WriteLine();
+
+            garage.PrintParkedVehicles();
+
+            if (!garage.ParkCar(airplane))
+                Console.WriteLine("Failed parked airplane");
+            Console.WriteLine($"Garage count: " + garage.Count);
+            Console.WriteLine();
+
+            garage.PrintParkedVehicles();
+
+            //if (!garage.UnparkCar(car))
+            //    Console.WriteLine("Failed to unpark parked car");
+
+            if (!garage.UnparkCar(car))
+                Console.WriteLine("Failed to unpark parked car");
+
+            //garage.PrintGarage();
+
+            if (!garage.UnparkCar(car2))
+                Console.WriteLine("Failed to unpark parked car2");
+
+            garage.PrintParkedVehicles();
 
 
+
+            //if (!garage.ParkCar(airplane))
+            //    Console.WriteLine("Failed parking airplane");
+            //Console.WriteLine($"Garage count: " + garage.Count);
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine($"\nUnpark vehicle airplane");
+            //var unpark = garage.UnparkCar(airplane);
+            //if (!unpark)
+            //    Console.WriteLine("Failed unpark airplane!");
+            //Console.WriteLine($"Garage count: " + garage.Count);
+
+            //Console.WriteLine($"\nUnpark vehicle airplane - AGAIN");
+            //var unpark5 = garage.UnparkCar(airplane);
+            //if (unpark5)
+            //    Console.WriteLine("unparked airplane!");
+            //Console.WriteLine($"Garage count: " + garage.Count);
+
+            //Console.WriteLine($"\nUnpark vehicle car");
+            //var unpark2 = garage.UnparkCar(car);
+            //if (unpark2)
+            //{
+            //    Console.WriteLine("unparked car!");
+            //    Console.WriteLine($"Garage count: " + garage.Count);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("failed unpark!");
+            //}
+
+            Console.WriteLine($"Garage count: " + garage.Count);
+
+
+            //Console.WriteLine($"\nUnpark vehicle car");
+            //var unpark4 = garage.UnparkCar(car);
+            //if (unpark4)
+            //{
+            //    Console.WriteLine("unparked!");
+            //    Console.WriteLine($"Garage count: " + garage.Count);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("failed unpark!");
+            //}
+
+            Console.WriteLine($"Garage count => " + garage.Count);
         }
+
+
+
+
+
     }
 }
